6d163463b7dbdd5daf729830a2ae8588fc30d71a
Suryaansh-Jain <91973466+Suryaansh-Jain@users.noreply.github.com>
2022-04-16 17:20:12 +0530

    Update main.py

--- codes/main.py
+++ codes/main.py
@@ -12,13 +12,20 @@ x1 = np.linspace(0,5000,1000)
 y1 = x1*x1/100 + 100*x1 + 40
 y2 = 200*x1 - x1*x1/400
 y3 = 100 - x1/40
-plt.plot(x1,y1,'blue')
-plt.plot(x1,y2, 'red')
-plt.plot(x1,y2-y1, 'yellow')
+
+plt.plot(x1, y3) #The solpe which is so small that it is not visible.
+
+plt.plot(x1,y1, label='$y = x^{2}/100 + 100x + 40$')
+plt.plot(x1,y2,label='$y = 200x - x^{2}/400$')
+plt.plot(x1,y2-y1, label='$y = 100 - x/40$')
 
 plt.plot(4000, 199960, marker = 'o', markersize = 5, markerfacecolor = 'blue')
 
 plt.axhline(y = 0,color = "black")
 plt.axvline(x = 0,color = "black")
 
+plt.xlabel("Number of units")
+plt.ylabel("Cost (in rupees)")
+plt.grid
+plt.legend(loc='best', prop={'size':11})
 plt.show()
