5a2cbe89bdf4d8733bfdfeee31a5626d31a1c06a
Suryaansh-Jain <91973466+Suryaansh-Jain@users.noreply.github.com>
2022-04-20 22:58:00 +0530

    Update main.py

--- codes/main.py
+++ codes/main.py
@@ -1,10 +1,5 @@
-import matplotlib.pyplot as plt
 import numpy as np
-
-###
-#### Suryaansh Jain
-#### cs21btech11057
-###
+import matplotlib.pyplot as plt
 
 plt.rcParams.update({'font.size': 15})
 x1 = np.linspace(0,5000,1000)
@@ -15,9 +10,9 @@ y3 = 100 - x1/40
 
 plt.plot(x1, y3) #The solpe which is so small that it is not visible.
 
-plt.plot(x1,y1, label='$y = x^{2}/100 + 100x + 40$')
-plt.plot(x1,y2,label='$y = 200x - x^{2}/400$')
-plt.plot(x1,y2-y1, label='$y = 100 - x/40$')
+plt.plot(x1,y1, label='$Total\ Cost = x^{2}/100 + 100x + 40$')
+plt.plot(x1,y2,label='$P(x) = 200x - x^{2}/400$')
+plt.plot(x1,y2-y1, label='$Total\ Profit = 100 - x/40$')
 
 plt.plot(4000, 199960, marker = 'o', markersize = 5, markerfacecolor = 'blue')
 
@@ -25,7 +20,7 @@ plt.axhline(y = 0,color = "black")
 plt.axvline(x = 0,color = "black")
 
 plt.xlabel("Number of units")
-plt.ylabel("Cost (in rupees)")
+plt.ylabel("Cost")
 plt.grid
 plt.legend(loc='best', prop={'size':11})
 plt.show()
